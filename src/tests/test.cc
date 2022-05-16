4071b2b30ce5c77a6a9b233904237afe9f49e6d8
Narzaru <yaroslahek@gmail.com>
2022-05-17 01:24:46 +0700

    Revert tests/test.cc changes along with an explanatory comment
    The header will be removed along with the change to the Makefile

--- src/tests/test.cc
+++ src/tests/test.cc
@@ -1,6 +1,10 @@
 #include <gtest/gtest.h>
 
+// !The header will be removed along
+// with the change to the Makefile
+#include "vector.cc"
+
 int main(int argc, char *argv[]) {
   testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  return RUN_ALL_TESTS();
 }
